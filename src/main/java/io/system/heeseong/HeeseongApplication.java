d99e86b916a9a42ec349488b667fd72cbd69f8ea
hshan <hhsung0120@naver.com>
2021-09-15 00:33:03 +0900

    Initial commit

---
+++ src/main/java/io/system/heeseong/HeeseongApplication.java
@@ -0,0 +1,16 @@
+package io.system.heeseong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class HeeseongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeeseongApplication.class, args);
+    }
+
+}
