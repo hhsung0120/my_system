d825cb8e9439903575452c6760ea56693de451bf
hshan <hhsung0120@naver.com>
2021-09-17 00:04:24 +0900

    사진 업로드 기능 구현중

--- src/main/java/io/system/heeseong/board/controller/BoardController.java
+++ src/main/java/io/system/heeseong/board/controller/BoardController.java
@@ -2,7 +2,12 @@ package io.system.heeseong.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.support.StandardMultipartHttpServletRequest;
+
+import java.nio.file.Paths;
 
 @Controller
 @RequestMapping("/boards")
@@ -17,4 +22,26 @@ public class BoardController {
     public String form(){
         return "/board/form";
     }
+
+    @PostMapping("/smartEditorUpload")
+    public String smartEditorUpload(StandardMultipartHttpServletRequest request) throws Exception{
+        System.out.println(request);
+
+        MultipartFile upload = request.getFile("upload");
+        System.out.println(request.getParameter("callback"));
+        //유틸 가지고 와서 처리하자
+        upload.transferTo(Paths.get("D:/upload/system/"+upload.getOriginalFilename()));
+        System.out.println(upload.getOriginalFilename());
+
+        String returnData = new StringBuilder()
+                .append(request.getParameter("callback"))
+                .append("?callback_func=" + request.getParameter("callback_func"))
+                .append("&bNewLine=true&sFileName="+"test.png" + "&sFileURL=" + "file:///D:/upload/system/test.png")
+                .toString()
+                ;
+
+        return "redirect:" + returnData;
+    }
+
+
 }
---
+++ src/main/java/io/system/heeseong/common/config/WebConfig.java
@@ -0,0 +1,27 @@
+package io.system.heeseong.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+    @Value("${path.default-upload-path}")
+    String fileUploadPath;
+
+    // 업로드 폴더 지정
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        System.out.println(fileUploadPath);
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file://"+fileUploadPath);
+    }
+	
+/*    // 인덱스 페이지 설정
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","/demo/demoLT");
+    }*/
+}
