78d7192173951e4331b668fecaef983f503a6a41
hshan <hhsung0120@naver.com>
2021-12-13 23:18:51 +0900

    test 계정 셋팅

--- src/test/java/io/system/heeseong/user/controller/AccountUserController.java
+++ src/test/java/io/system/heeseong/user/controller/AccountUserController.java
@@ -1,9 +1,7 @@
 package io.system.heeseong.user.controller;
 
-import io.system.heeseong.user.service.MenuService;
 import io.system.heeseong.common.util.CryptoUtil;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,25 +10,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @AutoConfigureMockMvc
 public class AccountUserController {
 
-    @Autowired
-    private MenuService menuService;
-
     @Test
     public void passwordHash() {
-        System.out.println(CryptoUtil.encryptPassword("gksgmltjd1!"));
+        System.out.println(CryptoUtil.encryptPassword("test"));
         System.out.println(CryptoUtil.encryptPassword("gksgmltjd1!"));
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         System.out.println(bCryptPasswordEncoder.encode("gksgmltjd1!"));
         System.out.println(bCryptPasswordEncoder.matches("gksgmltjd1!", "$2a$10$nGytNer4Aisc1G8GI6q8.Oya6eoq5sdfDCaS1SI1915ufn7iYyiAt4PW"));
-    }
-
-    @Test
-    public void getMenuList() {
-        //menuService.getMenuList();
-    }
-
-    @Test
-    public void setMenuList() {
-        menuService.setMenuList();
     }
 }
